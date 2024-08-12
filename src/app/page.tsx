88c8cf0cf852dc5fd557beb4de28da563f61b687
Shubham <sharmashubham6822@gmail.com>
2024-08-12 20:50:45 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import TopicsList from "@/components/TopicsList";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <TopicsList />
+    </>
+  )
+}
